e4fbe7768c14e14903c7dbcbeb4419b95a1cba0f
khaled ammar <khal00o00@outlook.sa>
2022-08-30 23:05:15 +0300

    update 30/8

---
+++ like counter/likecounter.js
@@ -0,0 +1,23 @@
+
+
+var button1 =document.querySelector(".p9l")
+var count=9
+function fun1(){
+    count++;
+    button1.innerText=count+"likes"
+}
+
+var button2 =document.querySelector(".p12l")
+var count=12
+function fun2(){
+    count++;
+    button2.innerText=count+"likes"
+}
+
+var button3 =document.querySelector(".p9l2")
+var count=9
+function fun3(){
+    count++;
+    button3.innerText=count+"likes"
+}
+
