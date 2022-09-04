aae3f94255aface05d00ab0afcf486522dc83ed1
khaled ammar <khal00o00@outlook.sa>
2022-09-04 20:59:11 +0300

    update 30/8

--- JAVA S/weather js/weather.js
+++ JAVA S/weather js/weather.js
@@ -1,8 +1,8 @@
-var ro= document.querySelector(".foot") ;
-function fun1 (){
-    ro.remove() ;
+var ro = document.querySelector(".foot");
+function fun1() {
+    ro.remove();
 }
-
+// ------------------------------------------------------
 // const tem=[
 //     {fer:'60',cel:'24'},
 //     {fer:'52',cel:'18'},
@@ -19,3 +19,51 @@ function fun1 (){
 //     temp.numContent
 //     =temp.num
 // }
+
+// -----------------------------------------------
+
+// var sp1_ = document.querySelector(".sp1") 
+// function fun2 (x) {
+//     // f = x * 1.8 + 32
+//     if (x.value =="f"){
+
+//         sp1_.innerText = Number(sp1_) * 1.8 + 32;
+//         console.log(typeof(sp1_) )
+//     }
+
+// }
+// ------
+var sp1_ = document.querySelector(".sp1");
+var sp2_ = document.querySelector(".sp2");
+var sp3_ = document.querySelector(".sp3");
+var sp4_ = document.querySelector(".sp4");
+var sp5_ = document.querySelector(".sp5");
+var sp6_ = document.querySelector(".sp6");
+var sp7_ = document.querySelector(".sp7");
+var sp8_ = document.querySelector(".sp8");
+
+function fun2(x) {
+
+    if (x.value == "f") {
+
+        sp1_.innerText = 75 + "°";
+        sp2_.innerText = 65 + "°";
+        sp3_.innerText = 80 + "°";
+        sp4_.innerText = 66 + "°";
+        sp5_.innerText = 69 + "°";
+        sp6_.innerText = 61 + "°";
+        sp7_.innerText = 78 + "°";
+        sp8_.innerText = 70 + "°";
+    }
+    else {
+        sp1_.innerText = 24 + "°";
+        sp2_.innerText = 18 + "°";
+        sp3_.innerText = 27 + "°";
+        sp4_.innerText = 19 + "°";
+        sp5_.innerText = 21 + "°";
+        sp6_.innerText = 16 + "°";
+        sp7_.innerText = 26 + "°";
+        sp8_.innerText = 21 + "°";
+    }
+
+}
